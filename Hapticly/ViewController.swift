b347db28ec8e3338393ee98ca48d556c9c0301c9
osdnk <micosa97@gmail.com>
2018-07-10 23:15:48 +0200

    works

--- Hapticly/ViewController.swift
+++ Hapticly/ViewController.swift
@@ -19,6 +19,34 @@ class ViewController: UIViewController {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
+  
+  @IBAction func impactLight(sender: UIButton) {
+    UIImpactFeedbackGenerator(style: .light).impactOccurred();
+  }
+
+  @IBAction func impactMedium(sender: UIButton) {
+    UIImpactFeedbackGenerator(style: .medium).impactOccurred();
+  }
+  
+  @IBAction func impactHeavy(sender: UIButton) {
+    UIImpactFeedbackGenerator(style: .heavy).impactOccurred();
+  }
+  
+  @IBAction func notificationSuccess(sender: UIButton) {
+    UINotificationFeedbackGenerator().notificationOccurred(.success);
+  }
+  
+  @IBAction func notificationWarning(sender: UIButton) {
+    UINotificationFeedbackGenerator().notificationOccurred(.warning);
+  }
+  @IBAction func notificationError(sender: UIButton) {
+    UINotificationFeedbackGenerator().notificationOccurred(.error);
+  }
+
+  @IBAction func selection(sender: UIButton) {
+    UISelectionFeedbackGenerator().selectionChanged();
+  }
+
 
 
 }
