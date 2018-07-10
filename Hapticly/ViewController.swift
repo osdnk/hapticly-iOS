ef089596441158587dc326630f08064fba4418fb
osdnk <micosa97@gmail.com>
2018-07-10 22:47:45 +0200

    Initial Commit

---
+++ Hapticly/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hapticly
+//
+//  Created by Michał Osadnik on 10.07.2018.
+//  Copyright © 2018 Michał Osadnik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
